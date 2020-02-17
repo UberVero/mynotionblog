2851c88388e09488eb0a96685beb590c2b2f91a2
Veronica <superh.uma.nbeing@gmail.com>
2020-02-16 19:07:38 -0800

    personalized message and links

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,16 +16,7 @@ export default () => (
         alt="ZEIT + Notion"
       />
       <h1>Hi, this is Veronica</h1>
-      <h2>
-        I just created this personal blog using Next.js + Notion'{' '}
-        <ExtLink
-          href="https://github.com/zeit/next.js/issues/9524"
-          className="dotted"
-          style={{ color: 'inherit' }}
-        >
-          SSG
-        </ExtLink>
-      </h2>
+      <h2>I just created this personal blog using Next.js + Notion</h2>
 
       <Features />
 
@@ -34,19 +25,13 @@ export default () => (
           This is a statically generated{' '}
           <ExtLink href="https://nextjs.org">Next.js</ExtLink> site with a{' '}
           <ExtLink href="https://notion.so">Notion</ExtLink> powered blog that
-          is deployed with <ExtLink href="https://zeit.co">ZEIT</ExtLink>. It
-          leverages some upcoming features in Next.js like{' '}
-          <ExtLink href="https://github.com/zeit/next.js/issues/9524">
-            SSG support
-          </ExtLink>{' '}
-          and{' '}
-          <ExtLink href="https://github.com/zeit/next.js/issues/8626">
-            built-in CSS support
-          </ExtLink>{' '}
-          which allow us to achieve all of the benefits listed above including
-          blazing fast speeds, great local editing experience, and always being
-          available!
+          is deployed with <ExtLink href="https://zeit.co">ZEIT</ExtLink>.
         </p>
+        <h3>
+          Head over to the <ExtLink href="blog">blog</ExtLink> to check out the
+          latest posts, or learn more about{' '}
+          <ExtLink href="https://veronica.fyi">the author</ExtLink>.
+        </h3>
       </div>
     </div>
   </>
